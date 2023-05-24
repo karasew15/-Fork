ee0a35beb61c89a50a2e7838b91ef031b5144de1
karasew15 <karasew1599@mail.ru>
2023-05-24 21:36:55 +0400

    Initial Commit

---
+++ Обучение/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Обучение
+//
+//  Created by Сергей Карасев on 24.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
